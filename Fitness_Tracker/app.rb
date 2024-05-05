4afea1f82ed3ef6a301561940e8a83efa8f6999d
Seisuke Fukuda <razerrmc163@gmail.com>
2024-05-05 15:24:51 +0900

    Added CSV file

--- Fitness_Tracker/app.rb
+++ Fitness_Tracker/app.rb
@@ -1,7 +1,8 @@
-require_relative "../Fitness-Tracker-Project/Models/workout.rb"
-require_relative "../Fitness-Tracker-Project/Repository/workout_repo.rb"
-require_relative "../Fitness-Tracker-Project/Controller/workout_controller.rb"
+require_relative "../Fitness_Tracker/Models/workout.rb"
+require_relative "../Fitness_Tracker/Repository/workout_repo.rb"
+require_relative "../Fitness_Tracker/Controller/workout_controller.rb"
 require_relative "./router.rb"
+file_path = "../Fitness_Tracker/workout.csv"
 
 # workout = Workout.new(workout_name: "Squats", sets: 3, reps: 10, weight: "15kg")
 # p workout
@@ -9,7 +10,7 @@ require_relative "./router.rb"
 # exercise = Exercise.new(exercise_name: "Leg Day", exercise_description: "Lezgo")
 # p exercise
 new_workout = Workout.new(workout_name: "Squat", sets: 3, reps: 10)
-workout_repo = WorkoutRepository.new("./csv/workout.csv")
+workout_repo = WorkoutRepository.new(file_path)
 controller = WorkoutController.new(workout_repo)
 
 router = Router.new(controller)
